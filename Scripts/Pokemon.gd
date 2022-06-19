defcf2833bc7de448605b3f35e23ac0bd585f316
OscarPindaro <oscar.pindaro@gmail.com>
2022-06-19 19:36:15 +0200

    importing correctly all the animation, still need to check different pokemons

--- Scripts/Pokemon.gd
+++ Scripts/Pokemon.gd
@@ -1,4 +1,5 @@
-extends KinematicBody2D
+extends Node2D
+
 
 # paths to resources
 var sprite_collab_path : String = "Images/SpriteCollab/"
@@ -9,74 +10,68 @@ var ANIM_DATA_FILENAME : String = "AnimData.json"
 var poke_num_dict : Dictionary 
 var anim_dict : Dictionary
 
-var a : AnimationPlayer
+
+export var pokemon_name : String = "" setget set_pk_name, get_pk_name
+
+onready var anim_player : AnimationPlayer = $AnimationPlayer
+
+enum Direction {DOWN, DOWN_RIGHT, RIGHT, UP_RIGHT,
+UP, UP_LEFT, LEFT, DOWN_LEFT }
+
+func set_pk_name(name : String):
+	pokemon_name = name
+
+func get_pk_name():
+	return pokemon_name
 
 func _init():
-	# load assocition between pokemon and folder name
+	# load pokemon-folder association
 	var file : File = File.new()
 	var file_name : String = sprite_collab_path + poke_num_dict_name
 	file.open(file_name, File.READ)
 	poke_num_dict = parse_json(file.get_as_text())
-	load_sprite_animation("Bulbasaur", "Walk")
 
-
-func load_sprite_animation(pokemon_name : String, anim_name : String):
-	pass
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var sprites = $Sprites.get_children()
+	for sprite in sprites:
+		var anim_name = sprite.get_name()
+		print(anim_name)
+		load_sprite_attributes(sprite, anim_name)
+		create_anim_player_track(sprite, anim_name)
+	
+		
+func load_sprite_attributes(sprite : Sprite, anim_name : String):
+	# error if pokemon not present
 	if !poke_num_dict.has(pokemon_name):
-			push_error("The pokemon "+ pokemon_name+
-			" is not present in the dictionary.")
-	# create paths and load anims info file
+		push_error("The pokemon "+ pokemon_name+
+		" is not present in the dictionary.")
+	
+	#animation loading
 	var folder_number = poke_num_dict[pokemon_name]
 	var sprite_path = sprite_collab_path + sprite_folder + folder_number + "/"
 	var json_anim_path = sprite_path + ANIM_DATA_FILENAME
 	var anim_data_file = File.new()
 	anim_data_file.open(json_anim_path, File.READ)
 	anim_dict = parse_json(anim_data_file.get_as_text())
-	# creation of a sprite
 	var file_name : String = "%s-Anim.png" % anim_name
-	var spritesheet : Sprite = Sprite.new()
-	add_child(spritesheet)
-	spritesheet.texture = load(sprite_path+file_name)
+	# load texture
+	var image = Image.new()
+	image.load(sprite_path+file_name)
+	var texture = ImageTexture.new()
+	texture.create_from_image(image, 0)
+	sprite.texture = texture
+	# load vertical and horizontal frame numbers
 	var frame_heigth = get_anim_property(anim_name, "FrameHeight").to_int()
 	var frame_width = get_anim_property(anim_name, "FrameWidth").to_int()
-	var hframes : int = spritesheet.texture.get_width() / frame_width
-	var vframes : int = spritesheet.texture.get_height() / frame_heigth
-	
-	var animation = Animation.new()
-	var track_index = animation.add_track(Animation.TYPE_VALUE)
-	animation.set_length(1)
-	animation.set_step(1/vframes)
-	var path = String(spritesheet.get_path())+ ":frame"
-	print(spritesheet.get_path())
-	animation.track_set_path(0, path)
-	
-	spritesheet.frame = 0
-	spritesheet.region_enabled = true
-	spritesheet.region_rect = Rect2(0,0,frame_width, frame_heigth)
-	spritesheet.position = Vector2(500,500)
-	
-	for i in vframes:
-		animation.track_insert_key(track_index, i*animation.get_step(), i)
-	animation.value_track_set_update_mode(track_index, Animation.UPDATE_DISCRETE)			
-	animation.set_loop(true)
-	
-	var aPlayer = AnimationPlayer.new()
-	add_child(aPlayer)
-	aPlayer.add_animation("walk", animation)
-	aPlayer.set_current_animation("walk")
-	aPlayer.play("walk")
-	a = aPlayer
-
-func _ready():
-	pass
-
-func _process(delta):
-	a.play("walk")
-
+	sprite.hframes = sprite.texture.get_width() / frame_width
+	sprite.vframes = sprite.texture.get_height() / frame_heigth
+	sprite.set_scale(Vector2(2,2))
+	sprite.visible = false
+	anim_data_file.close()
 
 func get_anim_property(anim_name : String, property_name : String) -> String:
 	var anims = anim_dict["AnimData"]["Anims"]["Anim"]
-	var property = null
 	for anim in anims:
 		if anim["Name"] == anim_name:
 			if anim.has("CopyOf"):
@@ -87,6 +82,34 @@ func get_anim_property(anim_name : String, property_name : String) -> String:
 	return ""
 	
 
+func create_anim_player_track(sprite : Sprite, anim_name : String):
+	# creates an animation from the sprite for each cardinal direction
+	for dir in Direction:
+		var animation = Animation.new()
+		var track_index = animation.add_track(Animation.TYPE_VALUE)
+		animation.set_length(1.0)
+		animation.set_step(1.0/sprite.hframes)
+		var sprite_path = String(sprite.get_path())+ ":frame"
+		animation.track_set_path(0, sprite_path)
+		
+		# anim creation
+		var starting_frame = Direction.get(dir)*sprite.hframes
+		var ending_frame = (Direction.get(dir)+1)*sprite.hframes
+		print(starting_frame," ",  ending_frame)
+		for i in range(starting_frame, ending_frame):
+			animation.track_insert_key(track_index, (i-starting_frame)*animation.get_step(), i)
+		animation.value_track_set_update_mode(track_index, Animation.UPDATE_CONTINUOUS)			
+		animation.set_loop(true)
+		
+		# adding animation to player
+		anim_player.add_animation(anim_name + "_" +dir, animation)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$Sprites/Walk.visible = true
+	if Input.is_action_pressed("move_down"):
+		anim_player.play("Walk_DOWN")
+	else:
+		anim_player.set_current_animation("Walk_LEFT")
+		anim_player.advance(delta)
+	print($Sprites/Walk.frame)
--- Scripts/Pokemon_old.gd
+++ Scripts/Pokemon_old.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends KinematicBody2D
 
 # paths to resources
 var sprite_collab_path : String = "Images/SpriteCollab/"
@@ -9,17 +9,19 @@ var ANIM_DATA_FILENAME : String = "AnimData.json"
 var poke_num_dict : Dictionary 
 var anim_dict : Dictionary
 
+var a : AnimationPlayer
+
 func _init():
 	# load assocition between pokemon and folder name
 	var file : File = File.new()
 	var file_name : String = sprite_collab_path + poke_num_dict_name
 	file.open(file_name, File.READ)
 	poke_num_dict = parse_json(file.get_as_text())
-	
-func _ready():	
-	var a =load_sprite_animation("Pikachu", "Attack")
-	
+	load_sprite_animation("Pikachu", "Walk")
+
+
 func load_sprite_animation(pokemon_name : String, anim_name : String):
+	pass
 	if !poke_num_dict.has(pokemon_name):
 			push_error("The pokemon "+ pokemon_name+
 			" is not present in the dictionary.")
@@ -32,34 +34,45 @@ func load_sprite_animation(pokemon_name : String, anim_name : String):
 	anim_dict = parse_json(anim_data_file.get_as_text())
 	# creation of a sprite
 	var file_name : String = "%s-Anim.png" % anim_name
-	self.texture = load(sprite_path+file_name)
+	var spritesheet : Sprite = Sprite.new()
+	add_child(spritesheet)
+	spritesheet.texture = load(sprite_path+file_name)
 	var frame_heigth = get_anim_property(anim_name, "FrameHeight").to_int()
 	var frame_width = get_anim_property(anim_name, "FrameWidth").to_int()
-	self.hframes = self.texture.get_width() / frame_width
-	self.vframes = self.texture.get_height() / frame_heigth
+	var hframes : int = spritesheet.texture.get_width() / frame_width
+	var vframes : int = spritesheet.texture.get_height() / frame_heigth
 	
-	var walkAnimation = Animation.new()
-	walkAnimation.add_track(0)
-	walkAnimation.set_length(1)
-	walkAnimation.set_step(1.0/vframes)
-
-	var path = String(self.get_path()) + ":frame"
-	walkAnimation.track_set_path(0, path)
-	var delta_step = walkAnimation.step
-	for i in hframes:
-		walkAnimation.track_insert_key(0, delta_step*i, i)
-	walkAnimation.value_track_set_update_mode(0, Animation.UPDATE_DISCRETE)
-	walkAnimation.loop = 1
+	var animation = Animation.new()
+	var track_index = animation.add_track(Animation.TYPE_VALUE)
+	animation.set_length(1)
+	animation.set_step(1/vframes)
+	var path = String(spritesheet.get_path())+ ":frame"
+	print(spritesheet.get_path())
+	animation.track_set_path(0, path)
 	
-	self.position = Vector2(400,400)
-
+	spritesheet.frame = 0
+	spritesheet.region_enabled = true
+	spritesheet.region_rect = Rect2(0,0,frame_width, frame_heigth)
+	spritesheet.position = Vector2(500,500)
+	
+	for i in vframes:
+		animation.track_insert_key(track_index, i*animation.get_step(), i)
+	animation.value_track_set_update_mode(track_index, Animation.UPDATE_DISCRETE)			
+	animation.set_loop(true)
+	
 	var aPlayer = AnimationPlayer.new()
-	aPlayer.add_animation("walk", walkAnimation)
+	add_child(aPlayer)
+	aPlayer.add_animation("walk", animation)
 	aPlayer.set_current_animation("walk")
 	aPlayer.play("walk")
-	add_child(aPlayer)
-	return aPlayer
-	
+	a = aPlayer
+
+func _ready():
+	pass
+
+func _process(delta):
+	a.play("walk")
+
 
 func get_anim_property(anim_name : String, property_name : String) -> String:
 	var anims = anim_dict["AnimData"]["Anims"]["Anim"]
@@ -73,7 +86,6 @@ func get_anim_property(anim_name : String, property_name : String) -> String:
 	push_error("The animation " + anim_name + " is not present")
 	return ""
 	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
