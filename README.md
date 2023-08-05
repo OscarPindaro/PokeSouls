20373c0f152320a2e1786df5c269188d020809dc
Oscar Pindaro <oscar.pindaro@gmail.com>
2023-08-05 18:05:15 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,16 @@
+ # Pokesouls
+
+ if you dont have the sprites, clone it from git
+
+ git clone https://github.com/OscarPindaro/SpriteCollab.git
+
+ or
+
+ git clone git@github.com:OscarPindaro/SpriteCollab.git
+
+
+ Then, you need to convert the files AnimData.xml i think
+
+ python Scripts/python/ConvertAnimData.py
+
+ Lastly, you need a file that gives the corrispondesce between pokemon and numbers
--- Scripts/python/ConvertAnimData.py
+++ Scripts/python/ConvertAnimData.py
@@ -1,15 +1,21 @@
 import xmltodict, json, os
 
 BASE_PATH = "."
+# where the sprites are
 sprite_path = os.path.join(BASE_PATH, "Images", "SpriteCollab", "sprite")
+# content of sprite directory
 dirs = os.listdir(sprite_path)
 for name in dirs:
-    anim_data_path = os.path.join(sprite_path, name, "AnimData.xml")
-    anim_data_path_json = os.path.join(sprite_path, name, "AnimData.json")
-    with open(anim_data_path, "r") as f:
-        dict_anim_data = xmltodict.parse(f.read())
-        with open(anim_data_path_json, "w") as g:
-            json.dump(dict_anim_data, g, indent=2)
+    # ignore hidden files
+    if name.startswith("."):
+        pass
+    else:
+        anim_data_path = os.path.join(sprite_path, name, "AnimData.xml")
+        anim_data_path_json = os.path.join(sprite_path, name, "AnimData.json")
+        with open(anim_data_path, "r") as f:
+            dict_anim_data = xmltodict.parse(f.read())
+            with open(anim_data_path_json, "w") as g:
+                json.dump(dict_anim_data, g, indent=2)
 
 
 
